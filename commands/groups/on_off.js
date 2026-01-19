5e6537b6c6465d566d44883330661b06a53117f4
DevYerZx <yersonorosco2002@gmail.com>
2026-01-19 12:15:31 -0500

    Update on_off.js

--- commands/groups/on_off.js
+++ commands/groups/on_off.js
@@ -1,20 +1,24 @@
 module.exports = {
   command: ["antilink"],
   description: "Activa o desactiva funciones del grupo",
- categoria: "grupos",
+  categoria: "grupos",
   use: "antilink",
   isGroup: true,
   isAdmin: true,
   isBotAdmin: true,
+
   run: async (client, m, args) => {
     const cmd = m.text.trim().split(" ")[0].slice(1).toLowerCase();
     const setting = args[0]?.toLowerCase();
+
     if (!setting) {
       return m.reply(
-        "Debes especificar la *función*\n\n`Ejemplo`\n!on antilink\n!off antilink",
+        "Debes especificar la función\n\nEjemplo:\n.on antilink\n.off antilink",
       );
     }
+
     const chatData = global.db.data.chats[m.chat];
+    if (!chatData) return m.reply("Error en la base de datos");
 
     switch (setting) {
       case "antilink":
@@ -26,9 +30,9 @@ module.exports = {
 
       default:
         m.reply(
-          "Opción no *válida*\n\n- Opciones:\n`antilink`\n\n\n> Ejemplo: .on antilink",
+          "Opción no válida\n\nOpciones:\nantilink\n\nEjemplo:\n.on antilink",
         );
-        break;
     }
   },
 };
+
